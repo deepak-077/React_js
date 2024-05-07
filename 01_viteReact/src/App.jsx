27e9c78903394a5810ea34e2a755f846e3c51a48
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-07 09:47:05 +0000

    custom react

--- 01_viteReact/src/App.jsx
+++ 01_viteReact/src/App.jsx
@@ -1,9 +1,15 @@
 
 function App() {
+  const userName=" |deepak";
   
 
   return (
-    <h1>React JS with vite</h1>
+    <h1>React JS with vite 
+      {userName} 
+       {/* this is known as evaluated expression as it is evaluated and we can't check it here 
+      because  in the end its converted to obejct and we can't write expressions inside  
+      they have to be declared outside */}
+    </h1>
     
   )
 }
--- 01_viteReact/src/main.jsx
+++ 01_viteReact/src/main.jsx
@@ -2,9 +2,63 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+//we can write our jsx here 
+function MyApp(){
+  return (
+    <div>
+      <h1>this is custom app !</h1>
+    </div>
+  )
+}
+
+//custom react 
+// we can pass our custom react but it doesn't runs as it needs its own method and we are not writing according to the method of react
+    
+let reactElement={
+  type:'a',
+  props:{
+      //it stores properties or attrributes in key:value pair
+      href:"https//google.com",
+      target:"_blank"
+  },
+  children: "click here to visit google"
+}
+
+
+//element 
+
+const anotherElement=(
+  <a href='https://google.com' target="_blank"> Link to Google</a>
+)
+
+const anotherUser=" Deepak"
+
+
+//creating our own object of type React
+let reactElement2=React.createElement(
+  "a", //tag name
+  {href:"https://google.com", target:"_blank"}, // attributes/properties 
+  "click here for google" ,//text 
+  anotherUser //variables are injected at last after the is made 
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    
+    <App/>
+    // <MyApp />
+    //now since MyApp is a function so we can simply write it as
+    // MyApp()
+
+    // we can pass our custom react but it doesn't runs as it needs its own method and we are not writing according to the method of react
+    // reactElement
+
+    //we can pass tags simply to verify its converting to Tree Structure
+    // anotherElement
+    
+    //calling our own react method
+    // reactElement2
+
+  
 )
