ab1e8d297dd28230b641826026fb4a1d09835214
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-08 09:09:18 +0000

    hooks project

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [counter,setCounter]=useState(15)
+  let [counter,setCounter]=useState(15)
 
   //we can pass any thing as default in useState("")/useState(true)/useState("helo")/useState({object})/
   // it returns 2 things in array format so we are storing it as array
@@ -15,13 +15,29 @@ function App() {
   let addValue=function(){
     console.log("value added",counter)
     counter++;
+    setCounter(counter)
     // counter is changing in console but its not updating in the html page
     // UI is controlled by react and it provides the functionality of hooks for UI updation
     // there are many hooks for various purposes
     // they are imported in the {useState} 
 
-    // useState- its responsible for propagating variable updation in UI / DOM
+    // useState- its responsible for propagating variable updation in UI / DOM, 
+    // it will update it in the entire page where counter is used
 
+  }
+
+  let removeValue=function(){
+    
+    if(counter==0){
+      
+    }
+    else{
+      setCounter(counter--)
+
+    }
+    
+    
+    
   }
 
   
@@ -31,10 +47,10 @@ function App() {
       <h1> MacBook Air</h1>
       <h2> Counter Value : {counter}</h2>
 
-      <button onClick={addValue}>Add Value</button>
+      <button onClick={addValue}> Add Value {counter}</button>
       {/* we are just passing reference because we want it to work only when clicked */}
       <br />
-      <button>Remove Value</button>
+      <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
