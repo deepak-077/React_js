4bdb9beb055f2718905773c1dd78b681e491d586
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-11 09:19:01 +0000

    changes

--- 05_password/src/App.jsx
+++ 05_password/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,7 +9,24 @@ function App() {
   const [charLAllowed,setCharAllowed]=useState(false);
   const [password,setPassword]=useState("")
 
-  const passwordGenerator = () =>{}
+  // useCallback(function,dependencies) - we can pass arrow function of in place of function
+
+  const passwordGenerator = useCallback(()=>{
+    let pass= ""
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if(numberAllowed) str+= "0123456789"
+    if(charLAllowed) str+= "!@#$%^&*()"
+
+    for(let i=1;i<=str.length();i++){
+      let charac =Math.floor(Math.random()*str.length()+1);
+
+      pass=str.chaarAt(charac);
+    }
+
+    setPassword(pass)
+    
+  },[lenght,numsLAllowed,charLAllowed,setPassword])
 
   return (
     <>
