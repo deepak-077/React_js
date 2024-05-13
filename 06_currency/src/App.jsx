27ab61cb204458e1a3ace1b3e33b910cd7a4e7ac
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-13 13:34:23 +0000

    currency convertor

---
+++ 06_currency/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import './index.css'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'> Currency app</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 06_currency/src/hooks/userCurrencyinfo.js
@@ -0,0 +1,10 @@
+import {useEffect, useState} from "react"
+
+//its a general naming convention for hooks to use prefix "use" 
+function useCurrencyInfo(currency){
+    //we want to call an api
+    useEffect(() =>{
+        fetch()
+    },[])
+
+}
