85ddb7fabff0a27d1334bb9a681e33a3a5db23ee
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-14 13:22:21 +0000

    react router

---
+++ 07_reactRouter/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import Home from './components/home/home'
+
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div>React Router</div>
+    </>
+  )
+}
+
+export default App
