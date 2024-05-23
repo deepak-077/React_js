77442b48c65683b053dd386e10145e0ecbcf6a8b
Deepak Kumar <125119545+deepak-077@users.noreply.github.com>
2024-05-23 13:08:51 +0000

    redux toolkit

---
+++ 12_redux/src/features/todoSlice.js
@@ -0,0 +1,50 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+// nano id generates unique ids
+
+// declaring initial state as objects to it can have multiple properties
+
+const initialState ={
+    todos:[{
+        id:1,
+        text:"hello world"
+    }]
+}
+
+export const todoSlice =createSlice({
+    name:'todo',
+    initialState,
+
+    // Reducers - it is a functionality. It contains properties and functions
+
+    reducers:{
+        // addTodo will have access to 2 things 
+        // State - it tells us about the current state
+        // Action - it provide data passed
+        addTodo: (state, action) =>{
+            const todo ={
+                id:nanoid(),
+                // passing whatever was passed in text
+                text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo: (state,action) =>{
+            // applying filter to remove the id that matches with one we get from action
+            state.todos = state.todos.filter((todo) => todo.id!==action.payload)
+
+        },
+
+        // this differentiates reducers from ContextApi - In context Api we only write functions and not its definition
+    }
+})
+
+// we will update state with the help of reducers, so exporting them
+
+export const {addTodo,removeTodo} =todoSlice.action
+
+// it will only update with the help of registered reducers so giving it a list of all reducers
+
+export default todoSlice.reducer
+
+
